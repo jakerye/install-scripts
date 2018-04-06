7455b6692f05c639ac7b5eb53faa66fadc4b4679
Jake Rye <jrye@mit.edu>
2018-04-06 12:20:06 +0000

    removed aliasing of rsub to subl

--- rsub.sh
+++ rsub.sh
@@ -5,8 +5,9 @@
 # Run on remote machine
 sudo wget -O /usr/local/bin/rsub https://raw.github.com/aurora/rmate/master/rmate
 sudo chmod +x /usr/local/bin/rsub
-echo alias subl=rsub >> ~/.bash_aliases
 
 # Logout, open Sublime (w/rsub installed) on local machine
 # Log back in with:
 # ssh -R 52698:localhost:52698 user@ip
+# Open remote file:
+# rsub myfile.txt
