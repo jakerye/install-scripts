f1fa4a4fbf9ab8814b161b0bcda0709c20064bc3
Jake Rye <jrye@mit.edu>
2018-06-12 08:44:19 -0400

    added script for setting static ip on bbb

---
+++ staticip.sh
@@ -0,0 +1,12 @@
+
+# https://wiki.openag.media.mit.edu/users/rbaynes/notes/bbb/debian?s[]=beaglebone&s[]=ip
+
+sudo connmanctl
+> services
+	*AR Wired                ethernet_e8eb11255fc3_cable
+> agent on
+> config ethernet_e8eb11255fc3_cable --autoconnect yes
+
+# config <service> --ipv4 manual <ip_addr> <netmask> <gateway> --nameservers <dns_server>
+> config ethernet_e8eb11255fc3_cable  --ipv4 manual 172.17.2.31 255.255.0.0 172.17.0.1  --nameservers 172.17.0.1 18.71.0.151 18.70.0.160 18.72.0.3
+> quit
