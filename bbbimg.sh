d423039f4f8b32d4630784ae452d072a940735c2
Jake Rye <jrye@mit.edu>
2018-08-01 13:41:54 -0400

    added notes for cloning bbb emmc image onto fresh SD card

---
+++ bbbimg.sh
@@ -0,0 +1,10 @@
+# 1. Boot BBB normally
+# 2. Insert blank SD card
+# 3. SSH into BBB
+# 4. Run flasher script on BBB
+sudo /opt/scripts/tools/eMMC/beaglebone-black-make-microSD-flasher-from-eMMC.sh
+# 5. Wait for script to finish and BBB leds to turn off
+# 6. Remove power from BBB
+# 7. Remove SD card from BBB
+# 8. Insert SD card into laptop, convert SD card to .iso file
+sudo mkisofs -o <name>.iso /media/<user>/rootfs/
