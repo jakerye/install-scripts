866070277e0e265de3b3f9d058b22fcf40c4849c
Jake Rye <jrye@mit.edu>
2018-12-13 13:15:19 -0500

    made remote sublime a one line command easy to copy paste

--- remote_sublime.sh
+++ remote_sublime.sh
@@ -3,8 +3,7 @@
 # Installs remote sublime for remote file editing in sublime text.
 
 # Run on remote machine
-sudo wget -O /usr/local/bin/rsub https://raw.github.com/aurora/rmate/master/rmate
-sudo chmod +x /usr/local/bin/rsub
+sudo wget -O /usr/local/bin/subl https://raw.github.com/aurora/rmate/master/rmate && sudo chmod +x /usr/local/bin/subl
 
 # Logout, open Sublime (w/rsub installed) on local machine
 # Log back in with:
